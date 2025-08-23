64d2092966916ac143997d8da595135a6fcaad1a
umair-hussain02 <bc230203492uhu@vu.edu.pk>
2025-08-23 20:08:23 +0500

    Add Basic configurations of web frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-5xl text-boldnpx shadcn@latest init">
+        Welcome to IPS LMS
+      </h1>
+      <Button variant={"secondary"}>Hello</Button>
+    </>
+  );
+}
+
+export default App;
